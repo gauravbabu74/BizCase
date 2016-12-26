8873850e6a9e91b99a5a4ee86be1631ee9060017
B2C Developer <ravic@biz2credit.com>
2016-12-26 09:20:55 +0000

    Changes made by B2C Developer (ravic@biz2credit.com).

--- BizCase/app/modules/logout/logout.component.js
+++ BizCase/app/modules/logout/logout.component.js
@@ -19,10 +19,9 @@ var LogoutComponent = (function () {
             selector: "ns-login",
             templateUrl: "logout.component.html"
         }), 
-        __metadata('design:paramtypes', [(typeof (_a = typeof router_1.Router !== 'undefined' && router_1.Router) === 'function' && _a) || Object])
+        __metadata('design:paramtypes', [router_1.Router])
     ], LogoutComponent);
     return LogoutComponent;
-    var _a;
 }());
 exports.LogoutComponent = LogoutComponent;
 //# sourceMappingURL=logout.component.js.map
