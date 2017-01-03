8c8abacef28d5c14f13dd38bb27775876364142a
B2C Developer <ravic@biz2credit.com>
2017-01-03 13:29:09 +0000

    Changes made by B2C Developer (ravic@biz2credit.com).

--- BizCase/app/app.component.js
+++ BizCase/app/app.component.js
@@ -56,10 +56,9 @@ var AppComponent = (function () {
             selector: "ns-main",
             template: "<page-router-outlet></page-router-outlet>"
         }), 
-        __metadata('design:paramtypes', [(typeof (_a = typeof core_1.NgZone !== 'undefined' && core_1.NgZone) === 'function' && _a) || Object])
+        __metadata('design:paramtypes', [core_1.NgZone])
     ], AppComponent);
     return AppComponent;
-    var _a;
 }());
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
